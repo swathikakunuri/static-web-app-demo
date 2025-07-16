6757c34b2cc84744cd31881cb71738e3a053143b
swathikakunuri69@gmail.com <swathikakunuri69@gmail.com>
2025-07-16 17:56:12 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -1,43 +1,53 @@
-
-resource "azurerm_resource_group" "terra" {
-  name     = "terra"
+resource "azurerm_resource_group" "rg" {
+  name     = "vin-rg"
   location = "East US"
 }
-resource "azurerm_resource_group" "storeterra" {
-    name     = "example-resources"
-    location = "East US"
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vin-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
- 
-resource "azurerm_storage_account" "saterra" {
-    name                     = "terrastorage238"
-    resource_group_name      = azurerm_resource_group.storeterra.name
-    location                 = azurerm_resource_group.storeterra.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "vin-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
- 
-resource "azurerm_storage_container" "sacontainer" {
-    name                  = "terra-container"
-    storage_account_name  = azurerm_storage_account.saterra.name
-    container_access_type = "private"
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vin-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
-resource "azurerm_virtual_network" "terra_vnet" {
-    name                = "terra-vnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.terra.location
-    resource_group_name = azurerm_resource_group.terra.name
-}
- 
-resource "azurerm_subnet" "subnet1" {
-    name                 = "subnet1"
-    resource_group_name  = azurerm_resource_group.terra.name
-    virtual_network_name = azurerm_virtual_network.terra_vnet.name
-    address_prefixes     = ["10.0.1.0/24"]
-}
- 
-resource "azurerm_subnet" "subnet2" {
-    name                 = "subnet2"
-    resource_group_name  = azurerm_resource_group.terra.name
-    virtual_network_name = azurerm_virtual_network.terra_vnet.name
-    address_prefixes     = ["10.0.2.0/24"]
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = "vin-win-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  size                  = "Standard_DS1_v2"
+  admin_username        = "winadmin"
+  admin_password        = "P@ssw0rd123"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  provision_vm_agent    = true
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
 }
